eaa3dfc9989c878b755fc2d71042bab5e1800503
magait <lloll1050@naver.com>
2024-10-02 19:26:04 +0900

    [이중보]
    -최초 Ssave project 설정

---
+++ src/main/java/com/jung/Ssave/SsaveApplication.java
@@ -0,0 +1,13 @@
+package com.jung.Ssave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jung/Ssave/SsaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jung.Ssave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

62c598b54c7286feabc17ae32b876b8ae264b763
magait <lloll1050@naver.com>
2024-11-05 21:36:48 +0900

    [이중보] -aws 배포를 위한 다시 코드 생성

--- src/main/java/com/jung/Ssave/SsaveApplication.java
+++ src/main/java/com/jung/Ssave/SsaveApplication.java
@@ -2,9 +2,10 @@ package com.jung.Ssave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SsaveApplication {
+public class SsaveApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsaveApplication.class, args);

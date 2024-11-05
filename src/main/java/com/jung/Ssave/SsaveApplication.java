b9385e78d139516c407fc36f599cde830be31641
magait <lloll1050@naver.com>
2024-11-05 21:32:32 +0900

    [이중보] - 잘못 관련된 배포코드 삭제

--- src/main/java/com/jung/Ssave/SsaveApplication.java
+++ src/main/java/com/jung/Ssave/SsaveApplication.java
@@ -2,10 +2,9 @@ package com.jung.Ssave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SsaveApplication extends SpringBootServletInitializer {
+public class SsaveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsaveApplication.class, args);
